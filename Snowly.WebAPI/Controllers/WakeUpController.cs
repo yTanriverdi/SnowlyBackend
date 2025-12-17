8b4a34e8f05e9f44c65755c3f47d1f61870ba8d8
yasin <tanriverdi.yasinn@gmail.com>
2025-12-17 14:31:08 +0300

    WakeUp Eklendi

---
+++ Snowly.WebAPI/Controllers/WakeUpController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Snowly.Application.Commands.FriendShipCommands.AcceptFriendShip;
+using Snowly.WebAPI.APIResponse;
+
+namespace Snowly.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WakeUpController : ControllerBase
+    {
+        [HttpGet("Wake")]
+        public IActionResult Wake()
+        {
+            return Ok(ApiResponse<bool>.SuccessResponse(true, "Kullanıma hazır", 200));
+        }
+    }
+}
